4c28312aca1aeebe41ea09c3835c46ffb349f3c5
Everrou <126096738+Everrou@users.noreply.github.com>
2023-03-31 20:16:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function qq1 (){
+
+   let num1 = Number( document.getElementById("12").value );
+   let num2 = Number( document.getElementById("13").value );
+
+var randomNumber = Math.floor(Math.random() * 100);
+document.getElementById("123").innerHTML = randomNumber;
+}
+
+
+
+
+
+
+
+
+
